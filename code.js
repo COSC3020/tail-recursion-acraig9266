bc3b693d129b6b83fc5f0d9ae1f530056a0f4a29
acraig9266 <158104649+acraig9266@users.noreply.github.com>
2024-09-15 03:49:32 -0600

    Create code.js

---
+++ code.js
@@ -0,0 +1,26 @@
+function fib(n) {
+	if (n == 0) {
+		return 0;
+	} else if (n == 1 || n == 2) {
+		return 1;
+	} else {
+		i = 3;
+		iMin1 = 1;
+		iMin2 = 0;
+		return fibHelp(n, i, iMin1, iMin2);
+	}
+}
+
+function fibHelp(n, i, iMin1, iMin2) {
+	if (i > n) {
+		return (iMin1 + iMin2);
+	} else {
+		tmp = iMin1;
+		iMin1 += iMin2;
+		iMin2 = tmp;
+		return fibHelp(n, i + 1, iMin1, iMin2);
+	}
+}
+
+
+console.log(fib(1000))
